a3f97a2c033ed88be01a9235a84e351b5a2161ab
Oddbjørn Mathias Nødland <onoedland@hotmail.com>
2025-11-29 09:21:18 +0100

    feat: 05_joinStrings, v1

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -7,19 +7,20 @@
  The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
 */
 
-// Add your code right below, good luck!
+let firstName = "Carlos";
+let lastName = "Stevenson";
+let birthYear = 1947;
+let thisYear = 1965;
 
-
-
-
+let greeting = `Hello! My name is ${firstName} ${lastName} and I am ${thisYear - birthYear} years old.`;
 
 // Do not change this
 module.exports = {
-  firstName: typeof firstName === 'undefined' ? undefined : firstName,
-  lastName: typeof lastName === 'undefined' ? undefined : lastName,
-  thisYear: typeof thisYear === 'undefined' ? undefined : thisYear,
-  birthYear: typeof birthYear === 'undefined' ? undefined : birthYear,
-  greeting: typeof greeting === 'undefined' ? undefined : greeting,
-  fullName: typeof fullName === 'undefined' ? undefined : fullName,
-  age: typeof age === 'undefined' ? undefined : age
-}
+  firstName: typeof firstName === "undefined" ? undefined : firstName,
+  lastName: typeof lastName === "undefined" ? undefined : lastName,
+  thisYear: typeof thisYear === "undefined" ? undefined : thisYear,
+  birthYear: typeof birthYear === "undefined" ? undefined : birthYear,
+  greeting: typeof greeting === "undefined" ? undefined : greeting,
+  fullName: typeof fullName === "undefined" ? undefined : fullName,
+  age: typeof age === "undefined" ? undefined : age,
+};
